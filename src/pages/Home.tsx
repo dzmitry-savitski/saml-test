bf026cd777a8c58729c8a5ebcb5def48d0976cd0
Dzmitry Savitski <dmitry.savitski@gmail.com>
2025-07-03 19:00:00 -0400

    Add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <div>Home Page (SP List)</div>;
+  return (
+    <>
+      <div>Home Page (SP List)</div>
+    </>
+  );
 };
 
 export default Home; 
